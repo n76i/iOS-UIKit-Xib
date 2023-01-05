09e57d8196b9954aa96990248fe5a2ae5c5e8786
Van Ngoc An <vanngocan@Van-Ngoc-An.local>
2023-01-05 09:53:04 +0700

    Test branch

---
+++ iOSUIKitXib/BuyDiamonds/TestVC/TestViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TestViewController.swift
+//  iOSUIKitXib
+//
+//  Created by Van Ngoc An  on 05/01/2023.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+   
+
+}
